1a6c885e7eb8f00a4b4bab7b28d03bc297a663d3
Randy Do <randyydoo@csu.fullerton.edu>
2023-03-04 18:24:14 -0800

    inOrder Traversal

--- trees.py
+++ trees.py
@@ -4,31 +4,63 @@ class Node:
       self.right = None
       self.value = value
     def insert(self, value):
-        if self.value:
+        # set as root
+        if self.value == None: 
+            self.value = value
             #check if less than
-            if value < self.value:
-                #go left and if none, insert node
-                if self.left == None:
-                    self.left == Node(value)
-                else:
-                    self.left.insert(value)
+        elif value < self.value:
+            #go left and if none, insert node
+            if self.left == None:
+                self.left == Node(value)
+            #if node is to left, we will start the insert function at the left which is the new "root"
+            else:
+                self.left.insert(value)
             #value is greater than root
-            elif value > self.value:
-                if self.right == None:
-                    self.right == Node(value)
-                else:
-                    self.right.insert(value)
-            else: 
-                self.value = value
+        else:
+            if self.right == None:
+                self.right == Node(value)
+            else:
+                self.right.insert(value)
+    #printing  tree in order
+        list = []
+        stack = []
+        cur = root
+        def inOrder(root):
+         #iterativly
+            while cur or stack is not None:  #to clear out stack and use frame to append to list when cur = none
+                while cur is not None:
+                    #add to stack until none
+                    stack.append(cur)
+                    cur = cur.left
+                cur = stack.pop()
+                list.append(cur.val)
+                #if cur.right == value then go into second frame of while loop to append to stack
+                cur = cur.right
+
+         
+         
+         
+         
+         #recursivly   
+            if not root:
+                return
+            inOrder(root.left)
+            list.append(root.value)
+            inOrder(root.right)
+        inOrder(root)
+        return list
 
 
-
-
-#create note
-a = Node("a")
-b = Node("b")
-c = Node("c")
-d = Node("d")
-e = Node("e")
-f = Node("f")
+#create root
+root = Node("g")
+root.insert("c")
+root.insert("b")
+root.insert("a")
+root.insert("e")
+root.insert("d")
+root.insert("f")
+root.insert("i")
+root.insert("h")
+root.insert("j")
+root.insert("k")
 
